61d6a24fae2aa827811000bf64067bf508823777
Anish Bharadwaj K G <124552306+anishkonodi@users.noreply.github.com>
2023-02-22 20:50:26 +0530

    Update Duplicate.java

--- day12/Duplicate.java
+++ day12/Duplicate.java
@@ -4,11 +4,11 @@ class Duplicate {
     int duplicateelement[] = new int[arr.length];
     int count = 0, duplicate = 0;
     for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr.length; j++) {
+      for (int j = i + 1; j < arr.length; j++) {
         if (arr[i] == arr[j]) {
           count = count + 1;
-          if (count > 1) {
-            duplicateelement[duplicate] = arr[i];
+          if (count == 1) {
+            duplicateelement[duplicate] = arr[j];
             duplicate++;
           }
         }
